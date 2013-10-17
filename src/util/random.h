165bcbc6bad8a6fde87b32a52fca502da14d90d1
Joel Walker <joel@walker9.net>
2013-10-16 23:09:33 -0500

    Added glew and fixed compile errors

--- src/util/random.h
+++ src/util/random.h
@@ -7,10 +7,18 @@
 class Random : public std::mt19937_64
 {
 public:
-    Random();
-    Random(unsigned int seed);
+    Random()
+        : Random(get_timestamp_seed())
+    {}
 
-    static unsigned int get_timestamp_seed();
+    Random(unsigned int seed)
+        : mersenne_twister_engine(seed)
+    {}
+
+    static unsigned int get_timestamp_seed()
+    {
+        return std::chrono::system_clock::now().time_since_epoch().count();
+    }
 };
 
 #endif // RANDOM_H
